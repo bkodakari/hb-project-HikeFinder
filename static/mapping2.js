61ec64ab66fd0a1ed6607b3ea54bf97910bbac65
vagrant <vagrant@vagrant.vm>
2017-07-04 06:52:32 +0000

    WIP working on getting latlng coordinates for map markers

--- static/mapping2.js
+++ static/mapping2.js
@@ -28,3 +28,23 @@ window.onload = function () {
     title: 'Hello World!'
   });
 }
+
+window.onload = function () {
+  // Create an array of alphabetical characters used to label the markers.
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  var markers = TrailLocationArray.map(function(location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length]
+    });
+  });
+
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(map, markers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+};
