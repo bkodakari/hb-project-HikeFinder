29837fee9445f95d5d946dc2e2e5b5b2ad8f765f
vagrant <vagrant@vagrant.vm>
2017-07-05 23:42:52 +0000

    working map markers need to get info links working need to refresh list of trails for each query

--- static/get_trail_list.js
+++ static/get_trail_list.js
@@ -2,7 +2,6 @@ $('#submit').on('click', getList);
 
 function getList(evt){
   evt.preventDefault();
-  console.log("*******do something, please");
   var formInputs = {
     'address':$('#address').val(),
     'distance':$('#distance').val()};
@@ -14,33 +13,35 @@ function loadListOfHikes(data){
   $.each(data, function(key, value){
     arrayTrailNames.push(key);
   });
-  console.log(data);
   
   var arrayTrailLatLng = [];
 
   for (var i=0; i < arrayTrailNames.length; i++){
-  console.log(arrayTrailNames[i]);
   var lat = data[arrayTrailNames[i]][1];
   var lng = data[arrayTrailNames[i]][2];
   var latLng = new google.maps.LatLng(lat, lng);
-  console.log(latLng);
   arrayTrailLatLng.push(latLng);
   document.getElementById('json_of_hikes').innerHTML += ('<li>'+'<a href="/trails/'+data[arrayTrailNames[i]][5]+'"">'+arrayTrailNames[i]+'</a></li>');
   }
 
-  console.log(arrayTrailLatLng);
+  var myLatlng = arrayTrailLatLng[0];
+  
+  var mapOptions = {
+    zoom: 11,
+    center: myLatlng
+  };
 
-  for (var l=0; l < arrayTrailLatLng.length; li++){
+  
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  for (var l=0; l < arrayTrailLatLng.length; l++){
     var marker = new google.maps.Marker({
     position: arrayTrailLatLng[l],
     title:"Hello World!"
   });
+  marker.setMap(map);
   }
-
 }
-
-
-
 
 function initMap() {
 
@@ -49,3 +50,9 @@ function initMap() {
     center: {lat: 37.7749, lng: -122.4194}
   });
 }
+
+
+
+
+
+
